2cc60b786864a759853f8768fff0994b1f075f31
carlos <linhos.cgonzalez@gmail.com>
2020-07-09 16:58:40 +0200

    Funcionalidad inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import {HomePage} from './pages/Home';
+import {NavBar} from './components/NavBar';
+import { SearchResults } from './components/SearchResults';
+import {SearchForm} from './components/SearchForm';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <hr />
+          <SearchForm />
+          <Switch>
+            <Route exact path="/search/:keyword" component={SearchResults}  />
+            <Route exact path="/:id?" component={HomePage}  />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
