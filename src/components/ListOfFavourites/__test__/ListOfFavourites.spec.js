a62b356132a5b2ec3c4b140c4c4d1bbf893d324c
carlos <linhos.cgonzalez@gmail.com>
2020-07-10 20:21:36 +0200

    fix list of favourites test

--- src/components/ListOfFavourites/__test__/ListOfFavourites.spec.js
+++ src/components/ListOfFavourites/__test__/ListOfFavourites.spec.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { ListOfFavourites } from '../index';
+import React from "react";
+import { render } from "@testing-library/react";
+import { ListOfFavourites } from "../index";
 
+describe("test suite for ListOfFavourites", () => {
+  test("renders learn react link", () => {
+    const useFavouritesMock = {
+      favourites: [],
+      loading: true,
+    };
 
-describe('test suite for ListOfFavourites', () => {
+    const { getByText } = render(
+      <ListOfFavourites
+        loading={useFavouritesMock.loading}
+        favourites={useFavouritesMock.favourites}
+      />
+    );
+    const linkElement = getByText(/Favoritos/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 
-    test('renders learn react link', () => {
-        const useFavouritesMock = {
-            favourites: [],
-            loading: true
-        }
+  test("show cargando... text while waiting for promise to resolve", () => {
+    const useFavouritesMock = {
+      favourites: [],
+      loading: true,
+    };
 
-        const { getByText } = render(<ListOfFavourites 
-                loading={useFavouritesMock.loading} 
-                favourites={useFavouritesMock.favourites}
-            />);
-        const linkElement = getByText(/Favoritos/i);
-        expect(linkElement).toBeInTheDocument();
-    });
+    const { container } = render(
+      <ListOfFavourites
+        loading={useFavouritesMock.loading}
+        favourites={useFavouritesMock.favourites}
+      />
+    );
 
-    test('show cargando... text while waiting for promise to resolve', () => {
-        const useFavouritesMock = {
-            favourites: [],
-            loading: true
-        }
+    expect(container.innerHTML).toMatch("<h6>cargando ...</h6>");
+  });
 
-        const {container} = render (
-            <ListOfFavourites 
-                loading={useFavouritesMock.loading} 
-                favourites={useFavouritesMock.favourites}
-            />)
-        
-        expect(container.innerHTML)
-        .toMatch('<h6>cargando ...</h6>')
-    })
+  test("show list of favourites when loading is false", () => {
+    const useFavouritesMock = {
+      favourites: [{ username: "titulo1" }, { username: "titulo2" }],
+      loading: false,
+    };
+    const { container } = render(
+      <ListOfFavourites
+        loading={useFavouritesMock.loading}
+        favourites={useFavouritesMock.favourites}
+      />
+    );
 
-    test('show list of favourites when loading is false', () => {
-        const useFavouritesMock = {
-            favourites: [{title: 'titulo1'}, {title: 'titulo2'}],
-            loading: false
-        }
-        const {container} = render(
-            <ListOfFavourites 
-                loading={useFavouritesMock.loading} 
-                favourites={useFavouritesMock.favourites}
-            />
-        )
-        expect(container.innerHTML)
-        .toMatch('Favoritos')
-    })
-})
+    expect(container.innerHTML).toMatch("titulo1");
+  });
+});
