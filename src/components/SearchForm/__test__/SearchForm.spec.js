ed3d2c67b1b2f562cbaf573bbd2234ca47303c8e
carlos <linhos.cgonzalez@gmail.com>
2020-07-12 11:02:26 +0200

    search Form

--- src/components/SearchForm/__test__/SearchForm.spec.js
+++ src/components/SearchForm/__test__/SearchForm.spec.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { SearchForm } from '../../SearchForm';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history'
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import { SearchForm } from "../../SearchForm";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
-test('renders learn react link', () => {
-    const history = createMemoryHistory()
-    const { getByText } = render(
-            <Router history={history}>
-                <SearchForm />
-            </Router>
-    );
-    
-    const linkElement = getByText(/Buscar/i);
-    expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={history}>
+      <SearchForm />
+    </Router>
+  );
+
+  const linkElement = getByText(/Buscar/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("search form could be used", async () => {
+  const { container } = render(<SearchForm />);
+  const input = await screen.findByRole("textbox");
 });
