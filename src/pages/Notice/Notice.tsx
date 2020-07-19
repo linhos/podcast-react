b2dfc27832acf11c27515ba9c7ac9e02d9077c18
carlos <linhos.cgonzalez@gmail.com>
2020-07-19 18:38:55 +0200

    Notice component

---
+++ src/pages/Notice/Notice.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Notice: React.FC = () => {
+    return (
+        <>
+            <h1>Avisos</h1>
+        </>
+    )
+}
+
+export default Notice;
---
+++ src/pages/Notice/__test__/Notice.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, cleanup, fireEvent, waitFor } from "@testing-library/react";
+import Notice from '../Notice'
+
+describe('test suite for Notice Page', () => {
+    const setup = () => render(<Notice />);
+    it('renders', () => {
+        const {asFragment} = setup();
+        expect(asFragment()).toMatchSnapshot();
+    })
+    it('should render the Notice title', async() => {
+        const {getByText} = setup();
+        
+        const title = getByText(/Avisos/i);
+        expect(title).toBeInTheDocument();
+    })
+})
